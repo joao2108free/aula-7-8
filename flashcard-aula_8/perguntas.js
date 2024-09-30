b9b432540beebbb88cdb0b6d079ac9b59d2c5c15
joao2108free <161027111+joao2108free@users.noreply.github.com>
2024-09-30 07:50:44 -0300

    aula 7 e 8

---
+++ flashcard-aula_8/perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'futebol',
+    'Que time ganhou a champions em 2009?',
+    'Futebol clube Barcelona'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Gabão',
+    'Libreville'
+)
+
+criaCartao(
+    'história',
+    'Que império Julio Cesar governou',
+    'Império romano'
+    
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz borboleta em Inglês?',
+    'butterfly'
+)
